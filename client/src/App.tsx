a5049b03c2f1a52ec8cf736195ea05565b94890a
Adnan <adnanxyz06@gmail.com>
2025-01-28 14:44:11 +0530

    Added shadcn/ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
       <h1>Hello world</h1>
+      <Button>Click me</Button>
     </>
   )
 }
